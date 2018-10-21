55fafb0b917361cb25849c7a083d87045ec37484
LundyBernard <lundy.bernard@gmail.com>
2018-10-21 16:55:01 -0500

    fix container_tests

--- cli.py
+++ cli.py
@@ -144,6 +144,24 @@ class ProjectCLI(object):
             help='port on which the service service will be run'
         )
 
+        # run_functional_tests args
+        run_container_tests = commands.add_parser(
+            'run_container_tests',
+            description='start docker-compose and run functional tests',
+            help='for details use test --help'
+        )
+        run_container_tests.set_defaults(func=self.run_container_tests)
+        run_container_tests.add_argument(
+            '-H', '--host', dest='host',
+            default='0.0.0.0',
+            help='host ip on which the service will be run',
+        )
+        run_container_tests.add_argument(
+            '-P', '--port', dest='port',
+            default='5000',
+            help='port on which the service service will be run'
+        )
+
         # example sub command
         sub1 = commands.add_parser(
             'sub1',
@@ -181,7 +199,7 @@ class ProjectCLI(object):
         start_server(host=args.host, port=args.port, debug=args.debug)
 
     def test(self, args):
-        print('run tests')
+        print('++ run functional tests ++')
         import unittest
         loader = unittest.TestLoader()
         suite = loader.discover('functional_tests', pattern='*_test.py')
@@ -194,10 +212,22 @@ class ProjectCLI(object):
         import signal
         from time import sleep
         a = subprocess.Popen(['project', 'start'])
-        sleep(1)
+        sleep(0.5)
         self.test(args)
 
         os.kill(a.pid, signal.SIGTERM)
+
+    def run_container_tests(self, args):
+        import subprocess
+        import os
+        import signal
+        from time import sleep
+        a = subprocess.Popen(['docker-compose', 'up'])
+        sleep(0.5)
+        self.test(args)
+
+        os.kill(a.pid, signal.SIGTERM)
+        sleep(0.5)
 
     def hello(self, args):
         print(hello_world())
--- project/tests/common_api_tests.py
+++ project/tests/common_api_tests.py
@@ -13,21 +13,3 @@ class CommonAPITest(object):
     def test_compose_webservice_exists(self):
         out = requests.get(self.service_address)
         self.assertEqual(out.text, 'Hello World!')
-
-    '''
-    def test_torch_cuda_is_available(self):
-        out = requests.get(self.service_address + 'cuda_is_available')
-        self.assertEqual(out.text, 'True')
-
-    def test_hello_i_am(self):
-        name = 'integration_tester'
-        out = requests.get(self.service_address + 'hello_i_am/' + name)
-        self.assertEqual(out.text, 'Hello integration_tester!\n')
-
-    def test_hello_i_am_api(self):
-        out = requests.get(
-            self.service_address + 'hello_i_am',
-            params={'name': 'alice'}
-        )
-        self.assertEqual(out.text, 'Hello alice!\n')
-    '''
